e55ed6b624a5742f33211a2e9f3c717fe4e6b424
Paulo <paulofcoelho20@gmail.com>
2023-02-23 13:22:21 -0800

    lastAnimation

--- index.js
+++ index.js
@@ -1,51 +1,27 @@
-gsap.from(".logo", {
-    x:-300, 
-    duration: 2,
-    // autoAlpha: 0,
-});
-gsap.from(".menu-links ul li", {
-    autoAlpha:0, 
-    // autoAlpha:1, 
-    duration:4,
-    stagger:0.1, 
-    delay:1,
-});
-gsap.to(".title", {
-    opacity:1, 
-    duration:3, 
-    delay:2,
-});
-gsap.to(".title", {
-    opacity:1, 
-    duration:3, 
-    delay:2,
-});
-gsap.to(".tagline",{
-    y: -40, 
-    autoAlpha:0, 
-    autoAlpha:1, 
-    duration:2, 
-    delay:2,
-});
-gsap.to(".desc",{
-    y: -40, 
-    autoAlpha:0, 
-    autoAlpha:1, 
-    duration:2, 
-    delay:2,
-});
-gsap.from(".beer",{
-    autoAlpha: 0,
-    delay: 4,
-    duration: 2,
-    y: -100, stagger:01,      
-});
 
-gsap.from(".beer img", {
-delay:5,
-y:20,
-ease: "BounceInOut",
-repeat: -1, // -1 infinity loops
-yoyo: true,
-repeatDelay: 3 // delay within each repeat)
+const TimeLine = gsap.timeline({
+    defaults: {
+        duration: 1,
+        autoAlpha: 0,
+        ease: "power4.out"
+    }
 })
+
+TimeLine
+.from('.logo', { x: -20 })
+.from('.menu-links ul li', { x: -20, stagger: 0.1 })
+.from('.title', { y: 20 }, "-=0.4")
+.from('.tagline', { y: 20 }, "-=0.7")
+.from('.desc', { y: 20 }, "<")
+.from('.beer', { y: -800 })
+.to('.beer', {
+    y: '16%',
+    // opacity: 1,
+    autoAlpha: 1,
+    yoyo: true,
+    repeat: -1,
+    ease: "power1.inOut",
+    duration: 2
+}, "+=0.1")
+
+
